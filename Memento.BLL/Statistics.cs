c7727eff36bcb0828e1682eca44d24191fe34b4a
Podoliak Artem <whiteworker777@gmail.com>
2020-10-25 01:14:55 +0300

    added statistics constructor

--- Memento.BLL/Statistics.cs
+++ Memento.BLL/Statistics.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Diagnostics;
 using Memento.DAL;
 
 namespace Memento.BLL
 {
     public class Statistics
     {
+        public Statistics()
+        {
+            TimeSpentToday = new TimeSpan();
+            AvarageTimePerDay = new TimeSpan();
+            CardsLearnedToday = new List<Card>();
+        }
+        public TimeSpan TimeSpentToday { get; set; }
+        public TimeSpan AvarageTimePerDay { get; set; }
+        public List<Card> CardsLearnedToday { get; set; }
 
     }
 }
