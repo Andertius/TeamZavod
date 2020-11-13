f06764d225ef86403559a6ffbb7db6db2f0330d2
Serhiy Pylypchuk <pylypchuk.serhiy@gmail.com>
2020-11-13 12:10:09 +0200

    Somethings in MainPage

--- Memento/UserControls/MainPageUserControl.xaml.cs
+++ Memento/UserControls/MainPageUserControl.xaml.cs
@@ -80,8 +80,14 @@ namespace Memento.UserControls
 
         public void StartEditing(object sender, RoutedEventArgs e)
         {
-            var deck = PickDeckCombox.SelectedItem as Deck;
-            StartEditingEvent?.Invoke(this, new StartEditingEventArgs(deck.Id));
+            if (PickDeckCombox.SelectedItem is Deck deck)
+            {
+                StartEditingEvent?.Invoke(this, new StartEditingEventArgs(deck.Id));
+            }
+            else
+            {
+                StartEditingEvent?.Invoke(this, new StartEditingEventArgs());
+            }
         }
 
         public void OpenSettings(object sender, RoutedEventArgs e)
